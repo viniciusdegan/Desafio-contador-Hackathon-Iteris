107e0a3fccc782d548f3b60c05dd9063b1c0bb86
Your Name <you@example.com>
2022-03-17 13:55:23 -0300

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DesafioContador/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import DesafioContador from './components/DesafioContador.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    DesafioContador,
   }
 }
 </script>
---
+++ src/components/DesafioContador.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="desafio-contador">
+    <h1>Contador: {{ contador }}</h1>
+    <!-- Definindo o número a ser inserido pelos botões -->
+
+    <button @click="somar">+</button>
+    <button @click="subtrair">-</button>
+    <!-- Inserindo a funcionalidade de click para somar e subtrair o número -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DesafioContador",
+
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    subtrair() {
+      this.contador--;
+    },
+    somar() {
+      this.contador++;
+    },
+  },
+}
+</script>
+<!-- O Script contém as funções para somar e subtrair o valor 1 do contador definido no HTML -->
+
+<style scoped>
+</style>
